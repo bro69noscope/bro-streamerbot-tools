16c06d1ea22165f32d75889c89dd8ab581ae549e
Loïc Villet <villet.loic@gmail.com>
2026-07-16 22:20:49 +0200

    make BRBManager more robust
    trying to avoid issue related to multiple overloads from `CPH.`

--- src/BroStreamerTools/BRBManager.cs
+++ src/BroStreamerTools/BRBManager.cs
@@ -1,20 +1,35 @@
 using System;
 using BroStreamerTools.Logging;
 
-namespace BroStreamerTools
-{
-    public static class BRBManager
-    {
-        public static void Start(object cph, int duration)
-        {
-            BroLogger.Info($"BRB started: {duration} minutes");
-            Invoke(cph, "SetGlobalVar", "BRBDuration", duration, true);
-            Invoke(cph, "SetGlobalVar", "BRBStartTime", DateTime.Now, true);
-        }
+namespace BroStreamerTools;
 
-        private static void Invoke(object cph, string method, params object[] args)
+public static class BRBManager
+{
+    public static void Start(object cph, string duration)
+    {
+        BroLogger.Info($"BRB started: {duration} minutes");
+
+        Invoke(cph, "SetGlobalVar", "BRBDuration", duration, true);
+        Invoke(cph, "SetGlobalVar", "BRBStartTime", DateTime.Now, true);
+
+        string scene = (string)Invoke(cph, "ObsGetCurrentScene", 0);
+
+        if (scene != "scene_lounge")
         {
-            cph.GetType().GetMethod(method).Invoke(cph, args);
+            Invoke(cph, "SetGlobalVar", "SkipAutoHide", true, true);
         }
+    }
+
+    private static object Invoke(object cph, string name, params object[] args)
+    {
+        var argTypes = Array.ConvertAll(args, a => a.GetType());
+
+        var method = cph.GetType().GetMethod(name, argTypes);
+
+        return method == null
+            ? throw new MissingMethodException(
+                $"Could not find {name}({string.Join(", ", Array.ConvertAll(argTypes, t => t.Name))})"
+            )
+            : method.Invoke(cph, args);
     }
 }
